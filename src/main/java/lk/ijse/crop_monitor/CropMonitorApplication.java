8a0481be48016026c4616c9eaa40861b05dea952
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-26 18:37:11 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/crop_monitor/CropMonitorApplication.java
@@ -0,0 +1,14 @@
+package lk.ijse.crop_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Bdu wda");
+		SpringApplication.run(CropMonitorApplication.class, args);
+	}
+
+}
