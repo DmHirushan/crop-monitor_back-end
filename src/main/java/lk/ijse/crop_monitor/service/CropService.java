972a26a454d9d2f0b8d73d366a8bbc5199a81cbd
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-28 21:14:35 +0530

    crop service layer and repository layer created

---
+++ src/main/java/lk/ijse/crop_monitor/service/CropService.java
@@ -0,0 +1,4 @@
+package lk.ijse.crop_monitor.service;
+
+public interface CropService {
+}
---
+++ src/main/java/lk/ijse/crop_monitor/service/impl/CropServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.crop_monitor.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.crop_monitor.service.CropService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CropServiceImpl implements CropService {
+}
