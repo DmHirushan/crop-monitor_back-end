d77591179aaa7093bbbb9249f3ae3c59aed2f4d3
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-28 20:56:17 +0530

    model mapper dependency and bean added

--- src/main/java/lk/ijse/crop_monitor/CropMonitorApplication.java
+++ src/main/java/lk/ijse/crop_monitor/CropMonitorApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.crop_monitor;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CropMonitorApplication {
@@ -11,5 +13,8 @@ public class CropMonitorApplication {
 		SpringApplication.run(CropMonitorApplication.class, args);
 	}
 
-
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
