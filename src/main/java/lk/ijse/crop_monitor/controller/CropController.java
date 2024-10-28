225228a35fc86d68c71b44fde5d5b9735f8ba1fb
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-28 23:10:57 +0530

    user save half way done

--- src/main/java/lk/ijse/crop_monitor/controller/CropController.java
+++ src/main/java/lk/ijse/crop_monitor/controller/CropController.java
@@ -2,21 +2,37 @@ package lk.ijse.crop_monitor.controller;
 
 import lk.ijse.crop_monitor.dto.impl.CropDto;
 import lk.ijse.crop_monitor.service.CropService;
+import lk.ijse.crop_monitor.util.AppUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/v1/crop")
 @CrossOrigin
+@RequiredArgsConstructor
 public class CropController {
-    @Autowired
-    private CropService cropService;
+    private final CropService cropService;
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveCrop(@RequestBody CropDto cropDto){
-        cropService.saveCrop(cropDto);
+    public ResponseEntity<Void> saveCrop(@RequestPart ("cropCode") String cropCode,
+                                         @RequestPart ("cropCommonName") String cropCommonName,
+                                         @RequestPart ("cropScientificName") String cropScientificName,
+                                         @RequestPart ("cropImage") MultipartFile cropImage,
+                                         @RequestPart ("category") String category,
+                                         @RequestPart ("cropSeason") String cropSeason,
+                                         @RequestPart ("fieldCode") String fieldCode,
+                                         @RequestPart ("cropDetailsCode") String cropDetailsCode
+
+    ) throws IOException {
+        CropDto cropDto = new CropDto();
+        byte[] crop = cropImage.getBytes();
+        String base64ProfilePic = AppUtil.toBase64ProfilePic(crop);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/lk/ijse/crop_monitor/util/AppUtil.java
+++ src/main/java/lk/ijse/crop_monitor/util/AppUtil.java
@@ -1,4 +1,9 @@
 package lk.ijse.crop_monitor.util;
 
+import java.util.Base64;
+
 public class AppUtil {
+    public static String toBase64ProfilePic(byte [] profilePic){
+        return Base64.getEncoder().encodeToString(profilePic);
+    }
 }
